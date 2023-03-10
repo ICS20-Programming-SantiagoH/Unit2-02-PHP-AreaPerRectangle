ff4c536423c2f1b0c15b2df6e7cbdf1f8e2da000
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-03-10 00:47:52 +0000

    Final index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    <meta charset="utf-8">
+    <meta name="description" content="My second webpage, with PHP">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Santiago Hewett">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Area & Perimeter of Rectangle in PHP</title>
+  </head>
+  <body>
+    <h1>Area & Perimeter of a Rectangle</h1>
+		<p>Length = 20 cm<br>Width = 32 cm</p>		
+		<p>The area of a rectangle with dimensions 20 cm x 32 cm = 640cm<sup>2</sup></p>
+    <p>The perimeter of a rectangle with dimensions 20 cm + 20 cm + 32 cm + 32 cm =104 cm</p>
+  </body>
+</html>
